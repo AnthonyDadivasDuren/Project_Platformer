a12632a2701474ca2734ff01e58575d9826a45a0
Anthony Durén <anthonydadivasduren@gmail.com>
2026-05-21 18:01:56 +0200

    Initial Commit

---
+++ Source/Project_Platformer/Project_Platformer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_Platformer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Platformer, "Project_Platformer" );
